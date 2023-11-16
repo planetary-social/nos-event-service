d0bb4b81f5aa4d09e200f54b90883bd569bd8805
boreq <boreq@0x46.net>
2023-11-16 16:03:21 +0100

    Validate if event should have been downloaded

--- service/app/handler_save_received_event.go
+++ service/app/handler_save_received_event.go
@@ -2,6 +2,7 @@ package app
 
 import (
 	"context"
+	"fmt"
 
 	"github.com/boreq/errors"
 	"github.com/planetary-social/nos-event-service/internal/logging"
@@ -46,9 +47,11 @@ func (h *SaveReceivedEventHandler) Handle(ctx context.Context, cmd SaveReceivedE
 		WithField("event.kind", cmd.event.Kind().Int()).
 		Message("saving received event")
 
-	if err := h.transactionProvider.Transact(ctx, func(ctx context.Context, adapters Adapters) error {
-		// todo check if event should be saved
+	if !h.shouldBeGloballyDownloaded(cmd.event.Kind()) {
+		return fmt.Errorf("event shouldn't have been downloaded, relay '%s' may be misbehaving", cmd.relay.String())
+	}
 
+	if err := h.transactionProvider.Transact(ctx, func(ctx context.Context, adapters Adapters) error {
 		if err := adapters.Events.Save(ctx, cmd.event); err != nil {
 			return errors.Wrap(err, "error saving the event")
 		}
@@ -63,4 +66,13 @@ func (h *SaveReceivedEventHandler) Handle(ctx context.Context, cmd SaveReceivedE
 	}
 
 	return nil
+}
+
+func (h *SaveReceivedEventHandler) shouldBeGloballyDownloaded(kind domain.EventKind) bool {
+	for _, v := range globalEventTypesToDownload {
+		if v == kind {
+			return true
+		}
+	}
+	return false
 }
