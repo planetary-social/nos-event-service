bab9eab99d0666823540609c4344d6e9eafbc38d
boreq <boreq@0x46.net>
2023-11-17 11:04:18 +0100

    Add more bootstrap relays

--- service/domain/relays/bootstrap_relay_source.go
+++ service/domain/relays/bootstrap_relay_source.go
@@ -8,9 +8,15 @@ import (
 )
 
 var bootstrapRelayAddresses = []domain.RelayAddress{
+	domain.MustNewRelayAddress("wss://relay.nostr.band"),
 	domain.MustNewRelayAddress("wss://relay.damus.io"),
 	domain.MustNewRelayAddress("wss://nos.lol"),
+	domain.MustNewRelayAddress("wss://e.nos.lol"),
+	domain.MustNewRelayAddress("wss://purplepag.es"),
 	domain.MustNewRelayAddress("wss://relay.current.fyi"),
+	domain.MustNewRelayAddress("wss://relay.nos.social"),
+	domain.MustNewRelayAddress("wss://relayable.org"),
+	domain.MustNewRelayAddress("wss://relay.snort.social"),
 }
 
 type BootstrapRelaySource struct {
