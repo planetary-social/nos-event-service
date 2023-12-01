97fb939842a25010c39c36d50003f87c61ba8c75
boreq <boreq@0x46.net>
2023-12-01 14:08:42 +0100

    Reduce memory usage and number of goroutines when sending events

--- service/domain/relays/relay_connection.go
+++ service/domain/relays/relay_connection.go
@@ -18,18 +18,43 @@ import (
 )
 
 const (
+	ReconnectionBackoff        = 5 * time.Minute
+	MaxDialReconnectionBackoff = 30 * time.Minute
+)
+
+const (
 	sendEventTimeout = 30 * time.Second
 )
+
+type Connection interface {
+	// SendMessage can be called concurrently.
+	SendMessage(msg NostrMessage) error
+
+	// ReadMessage can't be called concurrently.
+	ReadMessage() ([]byte, error)
+
+	// Close is idempotent.
+	Close() error
+}
+
+type NostrMessage interface {
+	MarshalJSON() ([]byte, error)
+}
 
 type BackoffManager interface {
 	GetReconnectionBackoff(err error) time.Duration
 }
 
+type ConnectionFactory interface {
+	Create(ctx context.Context) (Connection, error)
+	Address() domain.RelayAddress
+}
+
 type RelayConnection struct {
-	address        domain.RelayAddress
-	logger         logging.Logger
-	metrics        Metrics
-	backoffManager BackoffManager
+	connectionFactory ConnectionFactory
+	logger            logging.Logger
+	metrics           Metrics
+	backoffManager    BackoffManager
 
 	state      RelayConnectionState
 	stateMutex sync.Mutex
@@ -39,25 +64,25 @@ type RelayConnection struct {
 	subscriptionsUpdatedChClosed bool
 	subscriptionsMutex           sync.Mutex
 
-	eventsToSend      map[domain.EventId][]eventToSend
+	eventsToSend      map[domain.EventId]*eventToSend
 	eventsToSendMutex sync.Mutex
 	newEventsCh       chan domain.Event
 }
 
 func NewRelayConnection(
-	address domain.RelayAddress,
+	connectionFactory ConnectionFactory,
 	logger logging.Logger,
 	metrics Metrics,
 ) *RelayConnection {
 	return &RelayConnection{
-		address:                address,
-		logger:                 logger.New(fmt.Sprintf("relayConnection(%s)", address.String())),
+		connectionFactory:      connectionFactory,
+		logger:                 logger.New(fmt.Sprintf("relayConnection(%s)", connectionFactory.Address().String())),
 		metrics:                metrics,
 		backoffManager:         NewDefaultBackoffManager(),
 		state:                  RelayConnectionStateInitializing,
 		subscriptions:          make(map[transport.SubscriptionID]subscription),
 		subscriptionsUpdatedCh: make(chan struct{}),
-		eventsToSend:           make(map[domain.EventId][]eventToSend),
+		eventsToSend:           make(map[domain.EventId]*eventToSend),
 		newEventsCh:            make(chan domain.Event),
 	}
 }
@@ -69,7 +94,7 @@ func (r *RelayConnection) Run(ctx context.Context) {
 			r.logRunErr(err)
 		}
 
-		r.metrics.ReportRelayDisconnection(r.address, err)
+		r.metrics.ReportRelayDisconnection(r.connectionFactory.Address(), err)
 
 		select {
 		case <-ctx.Done():
@@ -87,7 +112,7 @@ func (r *RelayConnection) State() RelayConnectionState {
 }
 
 func (r *RelayConnection) Address() domain.RelayAddress {
-	return r.address
+	return r.connectionFactory.Address()
 }
 
 func (r *RelayConnection) GetEvents(ctx context.Context, filter domain.Filter) (<-chan EventOrEndOfSavedEvents, error) {
@@ -124,7 +149,21 @@ func (r *RelayConnection) SendEvent(ctx context.Context, event domain.Event) err
 	defer cancel()
 
 	ch := make(chan sendEventResponse)
-	r.scheduleSendingEvent(ctx, event, ch)
+	shouldNotify := r.scheduleSendingEvent(ctx, event, ch)
+
+	defer func() {
+		if err := r.removeEventChannel(event, ch); err != nil {
+			panic(err)
+		}
+	}()
+
+	if shouldNotify {
+		select {
+		case r.newEventsCh <- event:
+		case <-ctx.Done():
+			return ctx.Err()
+		}
+	}
 
 	select {
 	case <-ctx.Done():
@@ -137,29 +176,26 @@ func (r *RelayConnection) SendEvent(ctx context.Context, event domain.Event) err
 	}
 }
 
-func (r *RelayConnection) scheduleSendingEvent(ctx context.Context, event domain.Event, ch chan sendEventResponse) {
+func (r *RelayConnection) scheduleSendingEvent(ctx context.Context, event domain.Event, ch chan sendEventResponse) bool {
 	r.eventsToSendMutex.Lock()
 	defer r.eventsToSendMutex.Unlock()
 
-	r.eventsToSend[event.Id()] = append(r.eventsToSend[event.Id()], eventToSend{
-		ctx:   ctx,
-		ch:    ch,
-		event: event,
-	})
+	request := eventToSendRequest{
+		ctx: ctx,
+		ch:  ch,
+	}
 
-	go func() {
-		<-ctx.Done()
-		if err := r.removeEventChannel(event, ch); err != nil {
-			panic(err)
+	v, ok := r.eventsToSend[event.Id()]
+	if ok {
+		v.requests = append(v.requests, request)
+		return false
+	} else {
+		r.eventsToSend[event.Id()] = &eventToSend{
+			event:    event,
+			requests: []eventToSendRequest{request},
 		}
-	}()
-
-	go func() {
-		select {
-		case r.newEventsCh <- event:
-		case <-ctx.Done():
-		}
-	}()
+		return true
+	}
 }
 
 func (r *RelayConnection) logRunErr(err error) {
@@ -195,24 +231,29 @@ func (r *RelayConnection) removeSubscriptionChannel(chToRemove chan EventOrEndOf
 		}
 	}
 
-	return errors.New("somehow the channel was already removed")
+	return errors.New("somehow the channel was already removed?!")
 }
 
 func (r *RelayConnection) removeEventChannel(event domain.Event, chToRemove chan sendEventResponse) error {
 	r.eventsToSendMutex.Lock()
 	defer r.eventsToSendMutex.Unlock()
 
-	for i, eventToSend := range r.eventsToSend[event.Id()] {
-		if chToRemove == eventToSend.ch {
-			r.eventsToSend[event.Id()] = append(r.eventsToSend[event.Id()][:i], r.eventsToSend[event.Id()][i+1:]...)
-			if len(r.eventsToSend[event.Id()]) == 0 {
+	v, ok := r.eventsToSend[event.Id()]
+	if !ok {
+		return errors.New("this event wasn't scheduled to be sent?!")
+	}
+
+	for i, request := range v.requests {
+		if chToRemove == request.ch {
+			v.requests = append(v.requests[:i], v.requests[i+1:]...)
+			if len(v.requests) == 0 {
 				delete(r.eventsToSend, event.Id())
 			}
 			return nil
 		}
 	}
 
-	return errors.New("somehow the channel was already removed")
+	return errors.New("somehow the channel was already removed?!")
 
 }
 
@@ -236,7 +277,7 @@ func (r *RelayConnection) run(ctx context.Context) error {
 
 	r.logger.Trace().Message("connecting")
 
-	conn, err := NewConnection(ctx, r.address)
+	conn, err := r.connectionFactory.Create(ctx)
 	if err != nil {
 		return errors.Wrap(err, "error creating a connection")
 	}
@@ -251,10 +292,6 @@ func (r *RelayConnection) run(ctx context.Context) error {
 		}
 	}()
 
-	if err := r.queueResendingAllEvents(ctx); err != nil {
-		return errors.Wrap(err, "error queueing events")
-	}
-
 	go func() {
 		if err := r.manageSubs(ctx, conn); err != nil {
 			if !r.writeErrorShouldNotBeLogged(err) {
@@ -266,7 +303,7 @@ func (r *RelayConnection) run(ctx context.Context) error {
 	}()
 
 	go func() {
-		if err := r.sendNewEvents(ctx, conn); err != nil {
+		if err := r.sendEvents(ctx, conn); err != nil {
 			if !r.writeErrorShouldNotBeLogged(err) {
 				r.logger.Error().
 					WithError(err).
@@ -297,15 +334,17 @@ func (r *RelayConnection) writeErrorShouldNotBeLogged(err error) bool {
 }
 
 func (r *RelayConnection) handleMessage(messageBytes []byte) (err error) {
+	address := r.connectionFactory.Address()
+
 	envelope := nostr.ParseMessage(messageBytes)
 	if envelope == nil {
-		defer r.metrics.ReportMessageReceived(r.address, MessageTypeUnknown, &err)
+		defer r.metrics.ReportMessageReceived(address, MessageTypeUnknown, &err)
 		return errors.New("error parsing message, we are never going to find out what error unfortunately due to the design of this library")
 	}
 
 	switch v := envelope.(type) {
 	case *nostr.EOSEEnvelope:
-		defer r.metrics.ReportMessageReceived(r.address, MessageTypeEOSE, &err)
+		defer r.metrics.ReportMessageReceived(address, MessageTypeEOSE, &err)
 		r.logger.
 			Trace().
 			WithField("subscription", string(*v)).
@@ -318,7 +357,7 @@ func (r *RelayConnection) handleMessage(messageBytes []byte) (err error) {
 		r.passEventOrEOSEToChannel(subscriptionID, NewEventOrEndOfSavedEventsWithEOSE())
 		return nil
 	case *nostr.EventEnvelope:
-		defer r.metrics.ReportMessageReceived(r.address, MessageTypeEvent, &err)
+		defer r.metrics.ReportMessageReceived(address, MessageTypeEvent, &err)
 		r.logger.
 			Trace().
 			WithField("subscription", *v.SubscriptionID).
@@ -337,21 +376,21 @@ func (r *RelayConnection) handleMessage(messageBytes []byte) (err error) {
 		r.passEventOrEOSEToChannel(subscriptionID, NewEventOrEndOfSavedEventsWithEvent(event))
 		return nil
 	case *nostr.NoticeEnvelope:
-		defer r.metrics.ReportMessageReceived(r.address, MessageTypeNotice, &err)
+		defer r.metrics.ReportMessageReceived(address, MessageTypeNotice, &err)
 		r.logger.
 			Debug().
 			WithField("message", string(messageBytes)).
 			Message("received a message (notice)")
 		return nil
 	case *nostr.AuthEnvelope:
-		defer r.metrics.ReportMessageReceived(r.address, MessageTypeAuth, &err)
+		defer r.metrics.ReportMessageReceived(address, MessageTypeAuth, &err)
 		r.logger.
 			Debug().
 			WithField("message", string(messageBytes)).
 			Message("received a message (auth)")
 		return nil
 	case *nostr.OKEnvelope:
-		defer r.metrics.ReportMessageReceived(r.address, MessageTypeOK, &err)
+		defer r.metrics.ReportMessageReceived(address, MessageTypeOK, &err)
 		r.logger.
 			Trace().
 			WithField("message", string(messageBytes)).
@@ -370,7 +409,7 @@ func (r *RelayConnection) handleMessage(messageBytes []byte) (err error) {
 		r.passSendEventResponseToChannel(eventID, response)
 		return nil
 	default:
-		defer r.metrics.ReportMessageReceived(r.address, MessageTypeUnknown, &err)
+		defer r.metrics.ReportMessageReceived(address, MessageTypeUnknown, &err)
 		return errors.New("unknown message type")
 	}
 }
@@ -391,10 +430,15 @@ func (r *RelayConnection) passSendEventResponseToChannel(eventID domain.EventId,
 	r.eventsToSendMutex.Lock()
 	defer r.eventsToSendMutex.Unlock()
 
-	for _, eventToSend := range r.eventsToSend[eventID] {
+	v, ok := r.eventsToSend[eventID]
+	if !ok {
+		return
+	}
+
+	for _, request := range v.requests {
 		select {
-		case <-eventToSend.ctx.Done():
-		case eventToSend.ch <- response:
+		case <-request.ctx.Done():
+		case request.ch <- response:
 		}
 	}
 }
@@ -405,10 +449,7 @@ func (r *RelayConnection) setState(state RelayConnectionState) {
 	r.state = state
 }
 
-func (r *RelayConnection) manageSubs(
-	ctx context.Context,
-	conn *Connection,
-) error {
+func (r *RelayConnection) manageSubs(ctx context.Context, conn Connection) error {
 	defer conn.Close()
 
 	activeSubscriptions := internal.NewEmptySet[transport.SubscriptionID]()
@@ -428,7 +469,7 @@ func (r *RelayConnection) manageSubs(
 }
 
 func (r *RelayConnection) updateSubs(
-	conn *Connection,
+	conn Connection,
 	activeSubscriptions *internal.Set[transport.SubscriptionID],
 ) error {
 	r.subscriptionsMutex.Lock()
@@ -468,32 +509,29 @@ func (r *RelayConnection) updateSubs(
 		}
 	}
 
-	r.metrics.ReportNumberOfSubscriptions(r.address, len(r.subscriptions))
+	r.metrics.ReportNumberOfSubscriptions(r.connectionFactory.Address(), len(r.subscriptions))
 	return nil
 }
 
-func (r *RelayConnection) queueResendingAllEvents(ctx context.Context) error {
+func (r *RelayConnection) copyAllPastEvents() []domain.Event {
 	r.eventsToSendMutex.Lock()
 	defer r.eventsToSendMutex.Unlock()
 
-	for _, eventsToSend := range r.eventsToSend {
-		if len(eventsToSend) == 0 {
-			return errors.New("empty list which shouldn't happen as map entries get removed when list is empty")
-		}
-
-		event := eventsToSend[0].event
-		go func() {
-			select {
-			case <-ctx.Done():
-			case r.newEventsCh <- event:
-			}
-		}()
+	var result []domain.Event
+	for _, eventToSent := range r.eventsToSend {
+		result = append(result, eventToSent.event)
 	}
-
-	return nil
+	return result
 }
 
-func (r *RelayConnection) sendNewEvents(ctx context.Context, conn *Connection) error {
+func (r *RelayConnection) sendEvents(ctx context.Context, conn Connection) error {
+	for _, event := range r.copyAllPastEvents() {
+		msg := transport.NewMessageEvent(event)
+		if err := conn.SendMessage(msg); err != nil {
+			return errors.Wrap(err, "error writing a message")
+		}
+	}
+
 	for {
 		select {
 		case event := <-r.newEventsCh:
@@ -516,9 +554,13 @@ type subscription struct {
 }
 
 type eventToSend struct {
-	ctx   context.Context
-	ch    chan sendEventResponse
-	event domain.Event
+	event    domain.Event
+	requests []eventToSendRequest
+}
+
+type eventToSendRequest struct {
+	ctx context.Context
+	ch  chan sendEventResponse
 }
 
 type sendEventResponse struct {
@@ -592,11 +634,6 @@ func (t OKResponseError) Reason() string {
 	return t.reason
 }
 
-const (
-	ReconnectionBackoff        = 5 * time.Minute
-	MaxDialReconnectionBackoff = 30 * time.Minute
-)
-
 type DefaultBackoffManager struct {
 	consecutiveDialErrors int
 }
@@ -627,27 +664,23 @@ func (d *DefaultBackoffManager) isDialError(err error) bool {
 	return errors.Is(err, DialError{})
 }
 
-type NostrMessage interface {
-	MarshalJSON() ([]byte, error)
-}
-
-type Connection struct {
+type WebsocketConnection struct {
 	conn      *websocket.Conn
 	writeLock sync.Mutex
 }
 
-func NewConnection(ctx context.Context, address domain.RelayAddress) (*Connection, error) {
+func NewWebsocketConnection(ctx context.Context, address domain.RelayAddress) (*WebsocketConnection, error) {
 	conn, _, err := websocket.DefaultDialer.DialContext(ctx, address.String(), nil)
 	if err != nil {
 		return nil, NewDialError(err)
 	}
 
-	return &Connection{
+	return &WebsocketConnection{
 		conn: conn,
 	}, nil
 }
 
-func (c *Connection) SendMessage(msg NostrMessage) error {
+func (c *WebsocketConnection) SendMessage(msg NostrMessage) error {
 	msgJSON, err := msg.MarshalJSON()
 	if err != nil {
 		return errors.Wrap(err, "error marshaling message")
@@ -663,7 +696,7 @@ func (c *Connection) SendMessage(msg NostrMessage) error {
 	return nil
 }
 
-func (c *Connection) ReadMessage() ([]byte, error) {
+func (c *WebsocketConnection) ReadMessage() ([]byte, error) {
 	_, messageBytes, err := c.conn.ReadMessage()
 	if err != nil {
 		return nil, NewReadMessageError(err)
@@ -672,6 +705,22 @@ func (c *Connection) ReadMessage() ([]byte, error) {
 	return messageBytes, nil
 }
 
-func (c *Connection) Close() error {
+func (c *WebsocketConnection) Close() error {
 	return c.conn.Close()
+}
+
+type WebsocketConnectionFactory struct {
+	address domain.RelayAddress
+}
+
+func NewWebsocketConnectionFactory(address domain.RelayAddress) *WebsocketConnectionFactory {
+	return &WebsocketConnectionFactory{address: address}
+}
+
+func (w *WebsocketConnectionFactory) Create(ctx context.Context) (Connection, error) {
+	return NewWebsocketConnection(ctx, w.address)
+}
+
+func (w *WebsocketConnectionFactory) Address() domain.RelayAddress {
+	return w.address
 }
--- service/domain/relays/relay_connection_test.go
+++ service/domain/relays/relay_connection_test.go
@@ -1,13 +1,20 @@
 package relays_test
 
 import (
+	"context"
 	"fmt"
+	"sync"
 	"testing"
 	"time"
 
 	"github.com/boreq/errors"
+	"github.com/planetary-social/nos-event-service/internal"
 	"github.com/planetary-social/nos-event-service/internal/fixtures"
+	"github.com/planetary-social/nos-event-service/internal/logging"
+	"github.com/planetary-social/nos-event-service/service/domain"
 	"github.com/planetary-social/nos-event-service/service/domain/relays"
+	"github.com/planetary-social/nos-event-service/service/domain/relays/transport"
+	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 )
 
@@ -102,4 +109,151 @@ func TestDefaultBackoffManager_GetReconnectionBackoffResetsDialBackoffIfDifferen
 
 func TestDefaultBackoffManager_NormalBackoffIsSmallerThanMaxDialBackoff(t *testing.T) {
 	require.Less(t, relays.ReconnectionBackoff, relays.MaxDialReconnectionBackoff)
+}
+
+func TestRelayConnection_SendEventSendsEvents(t *testing.T) {
+	ctx := fixtures.TestContext(t)
+	ts := newTestConnection(t, ctx)
+
+	event := fixtures.SomeEvent()
+
+	go func() {
+		_ = ts.RelayConnection.SendEvent(ctx, event)
+	}()
+
+	require.EventuallyWithT(t, func(collect *assert.CollectT) {
+		msgs := ts.Connection.OutgoingMessages()
+		assert.Contains(collect, msgs, transport.NewMessageEvent(event))
+	}, 10*time.Second, 10*time.Millisecond)
+}
+
+type testConnection struct {
+	RelayConnection *relays.RelayConnection
+	Connection      *mockConnection
+}
+
+func newTestConnection(tb testing.TB, ctx context.Context) *testConnection {
+	connection := newMockConnection()
+	factory := newMockConnectionFactory(connection)
+	metrics := newMockMetrics()
+	logger := logging.NewDevNullLogger()
+	relayConnection := relays.NewRelayConnection(factory, logger, metrics)
+	go relayConnection.Run(ctx)
+
+	return &testConnection{
+		RelayConnection: relayConnection,
+		Connection:      connection,
+	}
+}
+
+type mockConnection struct {
+	closed bool
+	mutex  sync.Mutex
+
+	incomingMessages [][]byte
+	outgoingMessages []relays.NostrMessage
+}
+
+func newMockConnection() *mockConnection {
+	return &mockConnection{}
+}
+
+func (m *mockConnection) AddIncomingMessage(msg relays.NostrMessage) error {
+	b, err := msg.MarshalJSON()
+	if err != nil {
+		return errors.Wrap(err, "error marshaling the incoming message")
+	}
+
+	m.mutex.Lock()
+	defer m.mutex.Unlock()
+
+	m.incomingMessages = append(m.incomingMessages, b)
+	return nil
+}
+
+func (m *mockConnection) OutgoingMessages() []relays.NostrMessage {
+	m.mutex.Lock()
+	defer m.mutex.Unlock()
+
+	return internal.CopySlice(m.outgoingMessages)
+}
+
+func (m *mockConnection) SendMessage(msg relays.NostrMessage) error {
+	m.mutex.Lock()
+	defer m.mutex.Unlock()
+
+	if m.closed {
+		return errors.New("mock connection is closed")
+	}
+
+	m.outgoingMessages = append(m.outgoingMessages, msg)
+	return nil
+}
+
+func (m *mockConnection) ReadMessage() ([]byte, error) {
+	for {
+		m.mutex.Lock()
+
+		if m.closed {
+			return nil, errors.New("mock connection is closed")
+		}
+
+		if len(m.incomingMessages) > 0 {
+			msg := m.incomingMessages[0]
+			m.incomingMessages = m.incomingMessages[1:]
+			m.mutex.Unlock()
+			return msg, nil
+		}
+
+		m.mutex.Unlock()
+
+		<-time.After(100 * time.Millisecond)
+	}
+}
+
+func (m *mockConnection) Close() error {
+	m.mutex.Lock()
+	defer m.mutex.Unlock()
+
+	m.closed = true
+	return nil
+}
+
+type mockConnectionFactory struct {
+	conn    relays.Connection
+	address domain.RelayAddress
+}
+
+func newMockConnectionFactory(conn relays.Connection) *mockConnectionFactory {
+	return &mockConnectionFactory{
+		conn:    conn,
+		address: fixtures.SomeRelayAddress(),
+	}
+}
+
+func (m mockConnectionFactory) Create(ctx context.Context) (relays.Connection, error) {
+	return m.conn, nil
+}
+
+func (m mockConnectionFactory) Address() domain.RelayAddress {
+	return m.address
+}
+
+type mockMetrics struct {
+}
+
+func newMockMetrics() *mockMetrics {
+	return &mockMetrics{}
+}
+
+func (m2 mockMetrics) ReportRelayConnectionsState(m map[domain.RelayAddress]relays.RelayConnectionState) {
+}
+
+func (m2 mockMetrics) ReportNumberOfSubscriptions(address domain.RelayAddress, n int) {
+}
+
+func (m2 mockMetrics) ReportMessageReceived(address domain.RelayAddress, messageType relays.MessageType, err *error) {
+}
+
+func (m2 mockMetrics) ReportRelayDisconnection(address domain.RelayAddress, err error) {
 }
