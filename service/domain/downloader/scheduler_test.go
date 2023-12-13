e49923f4992eece40949d671090c783a28508f6e
boreq <boreq@0x46.net>
2023-12-13 19:41:55 +0100

    Tests

--- service/domain/downloader/scheduler_test.go
+++ service/domain/downloader/scheduler_test.go
@@ -3,9 +3,11 @@ package downloader_test
 import (
 	"context"
 	"slices"
+	"sync"
 	"testing"
 	"time"
 
+	"github.com/planetary-social/nos-event-service/internal"
 	"github.com/planetary-social/nos-event-service/internal/fixtures"
 	"github.com/planetary-social/nos-event-service/internal/logging"
 	"github.com/planetary-social/nos-event-service/service/adapters/mocks"
@@ -19,17 +21,18 @@ const (
 	numberOfTaskTypes                           = 3
 	testTimeout                                 = 10 * time.Second
 	delayWhenWaitingToConsiderThatTasksReceived = 5 * time.Second
+
+	waitFor = 10 * time.Second
+	tick    = 100 * time.Millisecond
 )
 
 func TestTaskScheduler_SchedulerWaitsForTasksToCompleteBeforeProducingMore(t *testing.T) {
 	t.Parallel()
 
-	ctx, cancel := context.WithTimeout(fixtures.TestContext(t), testTimeout)
-	defer cancel()
+	ctx := fixtures.TestContext(t)
+	ts := newTestedTaskScheduler(ctx, t)
 
 	start := date(2023, time.December, 27, 10, 30, 00)
-
-	ts := newTestedTaskScheduler(ctx, t)
 	ts.CurrentTimeProvider.SetCurrentTime(start)
 	ts.PublicKeySource.SetPublicKeys(downloader.NewPublicKeys(
 		[]domain.PublicKey{fixtures.SomePublicKey()},
@@ -39,8 +42,14 @@ func TestTaskScheduler_SchedulerWaitsForTasksToCompleteBeforeProducingMore(t *te
 	ch, err := ts.Scheduler.GetTasks(ctx, fixtures.SomeRelayAddress())
 	require.NoError(t, err)
 
-	tasks := collectAllTasks(t, ctx, ch, false)
-	require.Equal(t, numberOfTaskTypes, len(tasks))
+	tasks := collectAllTasks(ctx, ch, false)
+
+	require.EventuallyWithT(t, func(t *assert.CollectT) {
+		require.Equal(t, numberOfTaskTypes, len(tasks.Tasks()))
+	}, waitFor, tick)
+
+	<-time.After(5 * time.Second)
+	require.Len(t, tasks.Tasks(), numberOfTaskTypes)
 }
 
 func TestTaskScheduler_SchedulerDoesNotProduceEmptyTasks(t *testing.T) {
@@ -58,8 +67,10 @@ func TestTaskScheduler_SchedulerDoesNotProduceEmptyTasks(t *testing.T) {
 	ch, err := ts.Scheduler.GetTasks(ctx, fixtures.SomeRelayAddress())
 	require.NoError(t, err)
 
-	tasks := collectAllTasks(t, ctx, ch, false)
-	require.Equal(t, 1, len(tasks))
+	tasks := collectAllTasks(ctx, ch, false)
+
+	<-time.After(5 * time.Second)
+	require.Len(t, tasks.Tasks(), 1)
 }
 
 func TestTaskScheduler_SchedulerProducesTasksFromSequentialTimeWindowsLeadingUpToCurrentTime(t *testing.T) {
@@ -80,36 +91,38 @@ func TestTaskScheduler_SchedulerProducesTasksFromSequentialTimeWindowsLeadingUpT
 	ch, err := ts.Scheduler.GetTasks(ctx, fixtures.SomeRelayAddress())
 	require.NoError(t, err)
 
-	tasks := collectAllTasks(t, ctx, ch, true)
+	tasks := collectAllTasks(ctx, ch, true)
 
-	filters := make(map[downloader.TimeWindow][]domain.Filter)
-	for _, task := range tasks {
-		start := task.Filter().Since()
-		duration := task.Filter().Until().Sub(*start)
-		window := downloader.MustNewTimeWindow(*start, duration)
-		filters[window] = append(filters[window], task.Filter())
-	}
+	require.EventuallyWithT(t, func(t *assert.CollectT) {
+		filters := make(map[downloader.TimeWindow][]domain.Filter)
+		for _, task := range tasks.Tasks() {
+			start := task.Filter().Since()
+			duration := task.Filter().Until().Sub(*start)
+			window := downloader.MustNewTimeWindow(*start, duration)
+			filters[window] = append(filters[window], task.Filter())
+		}
 
-	firstWindowStart := date(2023, time.December, 27, 9, 30, 00)
-	var expectedWindows []downloader.TimeWindow
-	for i := 0; i < 59; i++ {
-		window := downloader.MustNewTimeWindow(firstWindowStart.Add(time.Duration(i)*time.Minute), 1*time.Minute)
-		expectedWindows = append(expectedWindows, window)
-	}
+		firstWindowStart := date(2023, time.December, 27, 9, 30, 00)
+		var expectedWindows []downloader.TimeWindow
+		for i := 0; i < 59; i++ {
+			window := downloader.MustNewTimeWindow(firstWindowStart.Add(time.Duration(i)*time.Minute), 1*time.Minute)
+			expectedWindows = append(expectedWindows, window)
+		}
 
-	var windows []downloader.TimeWindow
-	for window, filters := range filters {
-		require.Equal(t, numberOfTaskTypes, len(filters))
-		windows = append(windows, window)
-	}
+		var windows []downloader.TimeWindow
+		for window, filters := range filters {
+			assert.Equal(t, numberOfTaskTypes, len(filters))
+			windows = append(windows, window)
+		}
 
-	cmp := func(a, b downloader.TimeWindow) int {
-		return a.Start().Compare(b.Start())
-	}
+		cmp := func(a, b downloader.TimeWindow) int {
+			return a.Start().Compare(b.Start())
+		}
 
-	slices.SortFunc(expectedWindows, cmp)
-	slices.SortFunc(windows, cmp)
-	requireEqualWindows(t, expectedWindows, windows)
+		slices.SortFunc(expectedWindows, cmp)
+		slices.SortFunc(windows, cmp)
+		assertEqualWindows(t, expectedWindows, windows)
+	}, waitFor, tick)
 }
 
 func TestTaskScheduler_ThereIsOneWindowOfDelayToLetRelaysSyncData(t *testing.T) {
@@ -130,23 +143,25 @@ func TestTaskScheduler_ThereIsOneWindowOfDelayToLetRelaysSyncData(t *testing.T) 
 	ch, err := ts.Scheduler.GetTasks(ctx, fixtures.SomeRelayAddress())
 	require.NoError(t, err)
 
-	tasks := collectAllTasks(t, ctx, ch, true)
+	tasks := collectAllTasks(ctx, ch, true)
 
-	var windows []downloader.TimeWindow
-	for _, task := range tasks {
-		start := task.Filter().Since()
-		duration := task.Filter().Until().Sub(*start)
-		window := downloader.MustNewTimeWindow(*start, duration)
-		windows = append(windows, window)
-	}
+	require.EventuallyWithT(t, func(t *assert.CollectT) {
+		var windows []downloader.TimeWindow
+		for _, task := range tasks.Tasks() {
+			start := task.Filter().Since()
+			duration := task.Filter().Until().Sub(*start)
+			window := downloader.MustNewTimeWindow(*start, duration)
+			windows = append(windows, window)
+		}
 
-	slices.SortFunc(windows, func(a, b downloader.TimeWindow) int {
-		return a.Start().Compare(b.Start())
-	})
+		slices.SortFunc(windows, func(a, b downloader.TimeWindow) int {
+			return a.Start().Compare(b.Start())
+		})
 
-	require.Len(t, windows, 59)
-	lastWindow := windows[len(windows)-1]
-	require.Equal(t, date(2023, time.December, 27, 10, 29, 00), lastWindow.End().UTC())
+		assert.Len(t, windows, 59)
+		lastWindow := windows[len(windows)-1]
+		assert.Equal(t, date(2023, time.December, 27, 10, 29, 00), lastWindow.End().UTC())
+	}, waitFor, tick)
 }
 
 func TestTaskScheduler_TerminatesTasks(t *testing.T) {
@@ -240,28 +255,49 @@ func date(year int, month time.Month, day, hour, min, sec int) time.Time {
 	return time.Date(year, month, day, hour, min, sec, 0, time.UTC)
 }
 
-func requireEqualWindows(tb testing.TB, a, b []downloader.TimeWindow) {
-	require.Equal(tb, len(a), len(b))
+func assertEqualWindows(tb require.TestingT, a, b []downloader.TimeWindow) {
+	assert.Equal(tb, len(a), len(b))
 	for i := 0; i < len(a); i++ {
-		require.True(tb, a[i].Start().Equal(b[i].Start()))
-		require.True(tb, a[i].End().Equal(b[i].End()))
+		assert.True(tb, a[i].Start().Equal(b[i].Start()))
+		assert.True(tb, a[i].End().Equal(b[i].End()))
 	}
 }
 
-func collectAllTasks(t *testing.T, ctx context.Context, ch <-chan downloader.Task, ackAll bool) []downloader.Task {
-	var tasks []downloader.Task
-	for {
-		select {
-		case <-ctx.Done():
-			t.Fatal(ctx.Err())
-		case task := <-ch:
-			if ackAll {
-				go task.OnReceivedEOSE()
+type collectedTasks struct {
+	tasks []downloader.Task
+	l     sync.Mutex
+}
+
+func newCollectedTasks() *collectedTasks {
+	return &collectedTasks{}
+}
+
+func (c *collectedTasks) add(task downloader.Task) {
+	c.l.Lock()
+	defer c.l.Unlock()
+	c.tasks = append(c.tasks, task)
+}
+
+func (c *collectedTasks) Tasks() []downloader.Task {
+	c.l.Lock()
+	defer c.l.Unlock()
+	return internal.CopySlice(c.tasks)
+}
+
+func collectAllTasks(ctx context.Context, ch <-chan downloader.Task, ackAll bool) *collectedTasks {
+	v := newCollectedTasks()
+	go func() {
+		for {
+			select {
+			case <-ctx.Done():
+				return
+			case task := <-ch:
+				if ackAll {
+					go task.OnReceivedEOSE()
+				}
+				v.add(task)
 			}
-			tasks = append(tasks, task)
-		case <-time.After(delayWhenWaitingToConsiderThatTasksReceived):
-			t.Log("no new tasks for a short while, assuming that scheduler is gave us all tasks and waiting for the current ones to be completed")
-			return tasks
 		}
-	}
+	}()
+	return v
 }
