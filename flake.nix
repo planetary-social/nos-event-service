cf0bb2c19ef88b2ef7459b47719e8f6ce16e64a5
Zach Mandeville <code@solarpunk.cool>
2024-01-10 23:00:34 +1300

    Add flake for simple go dev environment

---
+++ flake.nix
@@ -0,0 +1,52 @@
+{
+  description = "Flake for developing on nos-event-service";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+    devshell.url = "github:numtide/devshell";
+  };
+
+  outputs = { self, nixpkgs, flake-utils, devshell }:
+    flake-utils.lib.eachDefaultSystem (system:
+    let pkgs = import nixpkgs {
+          inherit system;
+          overlays = [
+            devshell.overlays.default
+          ];
+        };
+    in
+      {
+        devShells.default =
+          pkgs.devshell.mkShell {
+            name = "nos-event-service";
+            packages = with pkgs; [
+              go
+              gopls
+              golangci-lint
+              gotools
+              go-tools
+            ];
+            commands = [
+              {
+                name = ''start'';
+                help = ''alias for `go run cmd/event-service/main.go`'';
+                category = ''dev helpers'';
+                command = ''
+                go run cmd/event-service/main.go
+                '';
+              }
+              {
+                name = ''lint'';
+                help = ''check style and formatting'';
+                category = ''dev helpers'';
+                command = ''
+                go vet ./...
+                golangci-lint run ./...
+                '';
+              }
+
+            ];
+          };
+      });
+}
